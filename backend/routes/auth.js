b80bdca3d79bc6ca19053f6f731b7581d5222e62
Danil Kochetov <danil.kochetov@gmail.com>
2018-05-03 20:16:56 +0300

    Add/remove project id to user on creation/deletion

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,23 +2,6 @@ const router = require('express').Router();
 const passport = require('passport');
 const createError = require('http-errors');
 
-/*router.get('/login', passport.authenticate('auth0', {}), (req, res) => {
-    res.redirect('//localhost:3000/');
-});
-
-router.get('/callback', passport.authenticate('auth0', {failureRedirect: '/login'}), (req, res) => {
-    if (!req.user) {
-        throw new Error('user null');
-    }
-
-    res.redirect('//localhost:3001/me');
-});
-
-router.get('/logout', (req, res) => {
-    req.logout();
-    res.redirect('//localhost:3000/');
-});*/
-
 router.get('/me', (req, res) => {
     if (req.isAuthenticated()) {
         res.end(JSON.stringify(req.user));
--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -1,19 +1,20 @@
 const router = require('express').Router();
 
 const Project = require('../models/Project');
+const User = require('../models/User');
 
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
     try {
-        new Project({
+        let project = await new Project({
             author_id: req.user._id,
             user_ids: [req.user._id],
             title: req.body.title,
             description: req.body.description,
-        }).save((err, project) => {
-            if (err) return next(err);
+        }).save();
 
-            res.end(JSON.stringify(project));
-        });
+        await User.updateOne({_id: req.user._id}, {$push: {project_ids: project._id}});
+
+        res.end(JSON.stringify(project));
     }
     catch (err) {
         console.error(err);
@@ -29,28 +30,35 @@ router.get('/:project_id', (req, res, next) => {
     });
 });
 
-router.put('/:project_id', (req, res, next) => {
-    Project.findByIdAndUpdate(req.params.project_id, req.body, {new: true}, (err, project) => {
+router.put('/:project_id', async (req, res, next) => {
+    try {
+        let project = await Project.findByIdAndUpdate(req.params.project_id, req.body, {new: true});
+
+        res.end(JSON.stringify(project));
+    }
+    catch (err) {
+        next(err);
+    }
+});
+
+router.delete('/:project_id', async (req, res, next) => {
+    try {
+        let project = await Project.findByIdAndRemove(req.params.project_id);
+        await User.updateOne({_id: project.author_id}, {$pull: {project_ids: project._id}});
+
+        res.end(JSON.stringify(project));
+    }
+    catch (err) {
+        next(err);
+    }
+});
+
+router.put('/:project_id/users/:user_id', (req, res, next) => {
+    Project.findOneAndUpdate(req.params.project_id, {$addToSet: {user_ids: req.params.user_id}}, {new: true}, (err, project) => {
         if (err) return next(err);
 
         res.end(JSON.stringify(project));
     });
-});
-
-router.delete('/:project_id', (req, res, next) => {
-    Project.findByIdAndRemove(req.params.project_id, (err, project) => {
-        if (err) return next(err);
-
-        res.end(JSON.stringify(project));
-    })
-});
-
-router.put('/:project_id/users/:user_id', (req, res, next) => {
-        Project.findOneAndUpdate(req.params.project_id, {$addToSet: {user_ids: req.params.user_id}}, {new: true}, (err, project) => {
-            if (err) return next(err);
-
-            res.end(JSON.stringify(project));
-        });
 });
 
 module.exports = router;
